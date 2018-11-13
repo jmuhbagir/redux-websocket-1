54cb305da732dfa6e7ca745d61d43e619f88b219
Jussi Kinnula <jussi.kinnula@wunderdog.fi>
2018-11-13 13:30:08 +0200

    Fix close function + bump version

--- src/websocket.ts
+++ src/websocket.ts
@@ -3,12 +3,14 @@ import { Config } from './config';
 let storedConfig: Config;
 let websocket: WebSocket;
 export function createConnection(config: Config = storedConfig) {
-  const { url, args, binaryType } = config;
-  if (!url) throw new Error('Cannot create WebSocket instance: no URL set');
-  websocket = new WebSocket(url, ...args);
-  storedConfig = config;
-  if (binaryType) websocket.binaryType = binaryType;
-  return websocket;
+  if (config) {
+    const { url, args, binaryType } = config;
+    if (!url) throw new Error('Cannot create WebSocket instance: no URL set');
+    websocket = new WebSocket(url, ...args);
+    storedConfig = config;
+    if (binaryType) websocket.binaryType = binaryType;
+    return websocket;
+  }
 }
 
 export function closeConnection() {
